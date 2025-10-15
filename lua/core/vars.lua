f8f40bc4e3e0a8146d2c2bcb0cf3e6ab560a526b
Nicolas Schmeltz <nschmeltz@pictet.com>
2025-10-15 09:15:44 +0200

    add lsp markdown + fix lua_ls

--- lua/core/vars.lua
+++ lua/core/vars.lua
@@ -1,2 +1,4 @@
 -- LSPs to install and configure
-vim.g.lsps = { "basedpyright", "lua_ls", "ruff", "stylua" }
+-- vim.g.lsps = { "basedpyright", "lua_ls", "ruff", "stylua" , "marksman"}
+vim.g.lsps = { "basedpyright", "lua-language-server", "ruff", "stylua" , "marksman"}
+
